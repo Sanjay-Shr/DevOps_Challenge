81aafbc77b5a302304d31f2f46ce758922cd0145
Sanjay <sanjaysharmau23@gmail.com>
2025-10-16 11:09:17 +0530

    Day-14-Shell-Scripting-Error-Handling

--- Day-14-Shell-Scripting-Error-Handling/error.sh
+++ Day-14-Shell-Scripting-Error-Handling/error.sh
@@ -6,43 +6,54 @@ and handle the code for errors
 task
 
 code_clone() {
-	echo "Cloning the Django app....."
-	git clone https://github.com/Sanjay-Shr/django-notes-app.git
+    echo "Cloning the Django app....."
+    if [ -d "django-notes-app" ]; then
+        echo "Directory already exists"
+    else
+        git clone https://github.com/Sanjay-Shr/django-notes-app.git
+    fi
 }
 
 install_requirements() {
-	echo "Installing dependencies"
-	sudo apt-get install docker.io nginx docker-compose -y
+    echo "Installing dependencies"
+    sudo apt-get update
+    sudo apt-get install -y docker.io nginx docker-compose
 }
 
 required_restarts() {
-	sudo chown $USER /var/run/docker.sock
-	sudo systemctl enable docker
-	sudo systemctl enable nginx
-	sudo systemctl restart docker
+    echo "Stopping host nginx to free port 80..."
+   # sudo systemctl stop nginx
+    #sudo systemctl disable nginx
+
+
+    echo "Setting up docker permissions..."
+    #sudo systemctl enable docker
+    #sudo systemctl restart docker
+    sudo chown $USER /var/run/docker.sock
 }
 
 deploy() {
-	docker build -t notes-app .
-	#docker run -d -p 8000:8000 notes-app:latest
-	docker-compose up -d
+    cd django-notes-app || exit
+    echo "Building Docker images and starting containers..."
+    docker build -t notes-app .
+    docker-compose up -d
 }
 
 echo "********** DEPLOYMENT STARTED **********"
-if ! code_clone; then
-	echo "the code directory already exists"
-	cd django-notes-app
-fi
-
-if ! install_requirements; then 
+code_clone
+if ! install_requirements; then
 	echo "Installation failed"
 	exit 1
 fi
-
 if ! required_restarts; then
 	echo "System fault identified"
 	exit 1
 fi
-deploy
+if ! deploy; then 
+	echo "Deployment failed, mailing admin"
+	# sendmail
+	exit 1
+fi
+
 echo "********** DEPLOYMENT ENDED **********"
 
